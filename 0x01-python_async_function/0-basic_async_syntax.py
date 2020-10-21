5791b89780a3221634423020292cd1413a16cbc2
Yesidh <944@holbertonschool.com>
2020-10-21 16:22:44 -0500

    [change order between document module and imports statements]

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import asyncio
-import random
 """
 ==============================================================================
 asynchronous coroutine that takes in an integer argument (max_delay, with
@@ -8,6 +6,8 @@ a default value of 10) named wait_random that waits for a random delay between
  0 and max_delay (included and float value) seconds and eventually returns it.
 ==============================================================================
 """
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
