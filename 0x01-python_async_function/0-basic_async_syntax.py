a4a969c9c84fdcc893c0f449e72d57794ac03b6e
Yesidh <944@holbertonschool.com>
2020-10-21 16:26:10 -0500

    [changing format documentation for the function]

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,10 +12,8 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-        Args:
-            max_delay: maximum delay time
-        Return:
-            float time interval
+    :param max_delay: maximum time delay
+    :return: random float value between 0-max_delay
     """
     random_value: float = random.uniform(0, max_delay)
     await asyncio.sleep(random_value)
