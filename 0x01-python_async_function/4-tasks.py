b9a3844791664c3dc0f7f325b54131de64c9a427
Yesidh <944@holbertonschool.com>
2020-10-21 23:41:36 -0500

    [task wait]

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ The code is nearly identical to wait_n except task_wait_random is being called.
 import asyncio
 from typing import List
 
-task_wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').wait_random
 
 
 async def task_await_n(n: int = 0, max_delay: int = 10) -> List[float]:
