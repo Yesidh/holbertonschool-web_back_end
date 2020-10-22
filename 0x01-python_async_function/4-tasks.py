3b3231c9c82598b7c5cb777672a3b9005f6bf619
Yesidh <944@holbertonschool.com>
2020-10-21 23:45:21 -0500

    [task wait]

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,7 +12,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_await_n(n: int = 0, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
         :param n: times to call wait_random function
         :param max_delay: maximum delay in each wait_random call
@@ -20,6 +20,7 @@ async def task_await_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
     list_float_random_values: List[float] = []
     list_float: List[asyncio.Task] = []
+
     for _ in range(n):
         list_float.append(task_wait_random(max_delay))
 
