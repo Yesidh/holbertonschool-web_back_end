8b09eeb24fe62c90b7e3a229683d86f8242e3146
Yesidh <944@holbertonschool.com>
2020-10-23 15:18:47 -0500

    [adding return statement into function]

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,10 @@ yield a random number between 0 and 10. Use the random module.
 """
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
         :return: a generator
     """
