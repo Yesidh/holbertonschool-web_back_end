bf0deb90ace3189ecca4414d94a86f837f64a44b
Yesidh <944@holbertonschool.com>
2020-10-24 00:14:24 -0500

    [Basic annotations - concat]

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+===============================================================================
+ a type-annotated function concat that takes a string str1 and a string str2 as
+ arguments and returns a concatenated string
+===============================================================================
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    :param str1: first string
+    :param str2: second string
+    :return: concatenated strings
+    """
+    return str1 + str2
