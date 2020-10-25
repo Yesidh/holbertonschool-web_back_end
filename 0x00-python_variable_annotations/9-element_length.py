fe2d05ba23c0ca84a52016d42634f192b6c31022
Yesidh <944@holbertonschool.com>
2020-10-24 23:06:42 -0500

    [Let's duck type an iterale object]

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+===============================================================================
+Annotate the below function’s parameters and return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+===============================================================================
+"""
+
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    :param lst: Iterator element
+    :return: a list of tuples, each tuple with a iterator and a int
+    """
+
+    return [(i, len(i)) for i in lst]
