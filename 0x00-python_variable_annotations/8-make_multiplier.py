09aa445fbeac9a76e618932f28df6bccf4157299
Yesidh <944@holbertonschool.com>
2020-10-24 22:44:09 -0500

    [Comples types - functions]

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+===============================================================================
+a type-annotated function make_multiplier that takes a float multiplier as
+argument and returns a function that multiplies a float by multiplier.
+===============================================================================
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+        :param multiplier:
+        :return: a function that multiplies two values
+    """
+
+    def multiply(f: float) -> float:
+        """
+        :param f: float value
+        :return: f * multiplier
+        """
+        return f * multiplier
+    
+    return multiply
