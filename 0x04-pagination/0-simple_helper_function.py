d7ddc6def55fdf06d6c6f661d0135d3c244dac0a
Yesidh <944@holbertonschool.com>
2020-10-28 16:24:48 -0500

    [simple pagination]

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -15,11 +15,11 @@ Page numbers are 1-indexed, i.e. the first page is page 1.
 from typing import Tuple
 
 
-def index_range(page: int = 10, page_size: int = 0) -> Tuple[int, int]:
+def index_range(page: int = 0, page_size: int = 0) -> Tuple[int, int]:
     """
-        :param page:
-        :param page_size:
-        :return:
+        :param page: page number to calculated the starting index
+        :param page_size: index size per page
+        :return: a tuple with starting and end index for a particular page
     """
 
     start_index = (page_size * page) - page_size
